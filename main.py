d6014a4de3f31c2486274e69837602728c3c55dd
Hao Zhang <hzhangxyz@outlook.com>
2021-01-18 22:32:38 +0800

    reconstruct python part of EOM loop

--- main.py
+++ main.py
@@ -24,93 +24,208 @@ from matrix_U import matrix_U
 Tensor = TAT(complex)
 
 
-def get_U(n, r, omega, phi, psi):
+def get_U(n, c, r, omega, phi, psi):
+    """
+    根据矩阵截断, 物理截断, r, omega, phi, psi创建U矩阵
+    """
     result = Tensor(["I1", "I2", "O1", "O2"], [n, n, n, n])
-    result.block[{}] = matrix_U(n, n, r, omega, phi, psi)
+    result.block[{}] = matrix_U(n, c, r, omega, phi, psi)
     return result
 
 
-def get_site(ps, Us):
-    return sum([
-        p * U.edge_rename({
-            "I1": "1.I1",
-            "I2": "1.I2",
-            "O1": "1.O1",
-            "O2": "1.O2"
-        }).shrink({
-            "1.I2": 0
-        }).contract(
-            U.edge_rename({
-                "I1": "2.I1",
-                "I2": "2.I2",
-                "O1": "2.O1",
-                "O2": "2.O2"
-            }).shrink({
-                "2.I2": 0
-            }).conjugate(), set()) for p, U in zip(ps, Us)
-    ])
-
-def get_A(n, k, eta):
+def get_A(n, c, k, eta):
+    """
+    根据矩阵截断, 物理截断, k, eta创建A矩阵
+    """
     result = Tensor(["O", "I"], [n, n])
-    result.block[{}] = matrix_A(n, n, k, eta)
+    result.block[{}] = matrix_A(n, c, k, eta)
     return result
 
-def trace_mps(l, a, g = None, bs = None):
-    ah = a.shrink({"1.I1": 0, "2.I1": 0}).edge_rename({"1.O2": "A.1.O2", "2.O2": "A.2.O2"})
-    if bs:
-        ah = ah.contract(bs, {("1.O1", "1")}).edge_rename({"2": "1.O1"})
-    result = ah.trace({("1.O1", "2.O1")})
-    for t in range(1, l):
-        if g:
-            result = result.contract(g.edge_rename({"1.O": "A.1.O2", "2.O": "A.2.O2"}), {("A.1.O2", "1.I"), ("A.2.O2", "2.I")})
-        result = result.contract(a.edge_rename({"1.O2": "A.1.O2", "2.O2": "A.2.O2"}), {("A.1.O2", "1.I1"), ("A.2.O2", "2.I1")})
-        if bs:
-            result = result.contract(bs, {("1.O1", "1")}).edge_rename({"2": "1.O1"})
-        result = result.trace({("1.O1", "2.O1")})
-    return result.trace({("A.1.O2", "A.2.O2")})
 
-def contract_mps(l, a, b, ga = None, gb = None, bs = None):
-    ah = a.shrink({"1.I1": 0, "2.I1": 0}).edge_rename({"1.O2": "A.1.O2", "2.O2": "A.2.O2"})
-    bh = b.shrink({"1.I1": 0, "2.I1": 0}).edge_rename({"1.O2": "B.1.O2", "2.O2": "B.2.O2"})
-    if bs:
-        ah = ah.contract(bs, {("1.O1", "1")}).edge_rename({"2": "1.O1"}).contract(bs, {("2.O1", "1")}).edge_rename({"2": "2.O1"})
-    result = ah.contract(bh, {("1.O1", "1.O1"), ("2.O1", "2.O1")});
-    for t in range(1, l):
-        if ga:
-            result = result.contract(ga.edge_rename({"1.O": "A.1.O2", "2.O": "A.2.O2"}), {("A.1.O2", "1.I"), ("A.2.O2", "2.I")})
-        if gb:
-            result = result.contract(gb.edge_rename({"1.O": "B.1.O2", "2.O": "B.2.O2"}), {("B.1.O2", "1.I"), ("B.2.O2", "2.I")})
-        result = result.contract(a.edge_rename({"1.O2": "A.1.O2", "2.O2": "A.2.O2"}), {("A.1.O2", "1.I1"), ("A.2.O2", "2.I1")})
-        if bs:
-            result = result.contract(bs, {("1.O1", "1")}).edge_rename({"2": "1.O1"}).contract(bs, {("2.O1", "1")}).edge_rename({"2": "2.O1"})
-        result = result.contract(
-            b.edge_rename({"1.O2": "B.1.O2", "2.O2": "B.2.O2"}),
-            {("B.1.O2", "1.I1"), ("B.2.O2", "2.I1"), ("1.O1", "1.O1"), ("2.O1", "2.O1")})
-    return result.trace({("A.1.O2", "B.1.O2"), ("A.2.O2", "B.2.O2")})
+def trace_one(length, As):
+    """
+    收缩一个长度为length的mpo表示的密度矩阵的迹
+    """
+    # mpo最左边的格点上下求迹
+    result = As[0].trace({("U", "D")})
+    for t in range(1, length):
+        # 第t个格点上下求迹
+        this = As[t].trace({("U", "D")})
+        # 第t个格点收缩到左边去
+        result = result.contract(this, {("R", "L")})
+    return result
 
-def main(l, n, r, omega, phi, psi, eta, delta, backward = 3):
-    U = get_U(n, r, omega, phi, psi)
-    S = get_site([1], [U])
-    sample = 5
-    Ss = get_site([1./(2*sample+1) for i in range(-sample, sample+1)],
-                  [get_U(n, r * (1 + i * delta / sample), omega, phi, psi) for i in range(-sample, sample+1)])
-    S /= S.norm_max()
-    Ss /= Ss.norm_max()
-    Aks = [get_A(n, k, eta) for k in range(n)]
-    gsite = sum([Ak.edge_rename({"I": "1.I", "O": "1.O"}).contract(Ak.conjugate().edge_rename({"I": "2.I", "O": "2.O"}), set()) for Ak in Aks])
-    bsite = Tensor(["1", "2"], [n, n]).zero()
-    for i in range(backward):
-        bsite[{"1":i, "2":i}] = 1
-    tracerhorhos = contract_mps(l, S, Ss, gb = gsite, bs = bsite)
-    tracerho = trace_mps(l, S, bs = bsite)
-    tracerhos = trace_mps(l, Ss, gsite, bs = bsite)
-    f = tracerhorhos / (tracerho * tracerhos)
-    print(f"Fidelity is {complex(f).real}")
+
+def trace_two(length, As, Bs):
+    """
+    收缩两个长度为length的mpo表示的密度矩阵的乘积的迹
+    """
+    # mpo最左端的右侧重命名为A.R, 另一个重命名为B.R, 他们的U和D相互缩并
+    result = As[0].edge_rename({
+        "R": "A.R"
+    }).contract(Bs[0].edge_rename({"R": "B.R"}), {("U", "D"), ("D", "U")})
+    for t in range(1, length):
+        # mpo的第t个格点按照最左端的规则重命名并收缩两个mpo的第t个格点
+        this = As[t].edge_rename({
+            "L": "A.L",
+            "R": "A.R"
+        }).contract(Bs[t].edge_rename({
+            "L": "B.L",
+            "R": "B.R"
+        }), {("U", "D"), ("D", "U")})
+        # 收缩到左边
+        result = result.contract(this, {("A.R", "A.L"), ("B.R", "B.L")})
+    return result
+
+
+def fidelity(length, As, Bs):
+    # 求迹前先归一化mpo
+    for i in range(length):
+        As[i] /= As[i].norm_max()
+        Bs[i] /= Bs[i].norm_max()
+    # 两个密度矩阵的fidelity = trace(a@b)
+    # 由于没有归一化, 所以一fidelity = trace(a@b) / (trace(a)trace(b))
+    trace_a = trace_one(length, As)
+    trace_b = trace_one(length, Bs)
+    trace_ab = trace_two(length, As, Bs)
+    f = trace_ab / (trace_a * trace_b)
+    return complex(f).real
+
+
+def build_from_UU(l, UU, n):
+    UU_with_I2_0 = UU.shrink({"UI2": 0, "DI2": 0})
+    result = []
+    result.append(
+        UU_with_I2_0.shrink({
+            "UI1": 0,
+            "DI1": 0
+        }).edge_rename({
+            "UO1": "U",
+            "DO1": "D"
+        }).merge_edge({"R": ["UO2", "DO2"]}))
+    middle_site = UU_with_I2_0.edge_rename({
+        "UO1": "U",
+        "DO1": "D"
+    }).merge_edge({
+        "L": ["UI1", "DI1"],
+        "R": ["UO2", "DO2"]
+    })
+    for t in range(1, l):
+        result.append(middle_site)
+    result.append(
+        Tensor(["UL", "DL", "U", "D"], [n, n, n, n]).identity({
+            ("UL", "U"), ("DL", "D")
+        }).merge_edge({"L": ["UL", "DL"]}))
+    return result
+
+
+def create_UU(n, c, r, omega, phi, psi):
+    U = get_U(n, c, r, omega, phi, psi)
+    UU = U.edge_rename({
+        "I1": "UI1",
+        "O1": "UO1",
+        "I2": "UI2",
+        "O2": "UO2"
+    }).conjugate().contract(
+        U.edge_rename({
+            "I1": "DI1",
+            "O1": "DO1",
+            "I2": "DI2",
+            "O2": "DO2"
+        }), set())
+    return UU
+
+
+def uniform(mean, delta):
+    if delta is None:
+        return [mean]
+    else:
+        sample = 5
+        return [
+            mean * (1 + delta * i / sample)
+            for i in range(-sample, sample + 1)
+        ]
+
+
+def build_chain(l,
+                n,
+                c,
+                r,
+                omega,
+                phi,
+                psi,
+                delta_r=None,
+                delta_omega=None,
+                delta_phi=None,
+                delta_psi=None):
+    UUs = []
+    for real_r in uniform(r, delta_r):
+        for real_omega in uniform(omega, delta_omega):
+            for real_phi in uniform(phi, delta_phi):
+                for real_psi in uniform(psi, delta_psi):
+                    UUs.append(
+                        create_UU(n, c, real_r, real_omega, real_phi,
+                                  real_psi))
+    UU = sum(UUs) / len(UUs)
+    return build_from_UU(l, UU, n)
+
+
+def create_kraus(n, c, eta):
+    Aks = [get_A(n, c, k, eta) for k in range(n)]
+    kraus = sum([
+        Ak.edge_rename({
+            "I": "UI",
+            "O": "UO"
+        }).conjugate().contract(Ak.edge_rename({
+            "I": "DI",
+            "O": "DO"
+        }), set()) for Ak in Aks
+    ])
+    return kraus.merge_edge({"L": ["UI", "DI"], "R": ["UO", "DO"]})
+
+
+def add_kraus(As, sites, kraus):
+    for i in sites:
+        As[i] = As[i].contract(kraus, {("R", "L")})
+
+
+def create_post(n, c=None, selected=None):
+    result = Tensor(["U", "D"], [n, n]).zero()
+    if c is not None:
+        if selected is not None:
+            pass
+        else:
+            for i in range(c):
+                result[{"U": i, "D": i}] = 1
+    else:
+        if select is not None:
+            for i in select:
+                result[{"U": i, "D": i}] = 1
+        else:
+            pass
+    return result
+
+
+def add_post(As, sites, post):
+    for i in sites:
+        As[i] = As[i].contract(post, {("U", "D")}).contract(post, {("D", "U")})
+
+
+def main(l, n, r, omega, phi, psi, eta, delta, post):
+    As = build_chain(l, n, n, r, omega, phi, psi)
+    Bs = build_chain(l, n, n,r, omega, phi, psi, delta_r=delta)
+    add_post(As, range(l + 1), create_post(n, post))
+    add_post(Bs, range(l + 1), create_post(n, post))
+    add_kraus(Bs, range(l - 1), create_kraus(n, n, eta))
+    print(fidelity(l + 1, As, Bs))
+
 
 if __name__ == "__main__":
     import fire
+
     def Display(lines, out):
         text = "\n".join(lines) + "\n"
         out.write(text)
+
     fire.core.Display = Display
     fire.Fire(main)
