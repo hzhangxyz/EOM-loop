f9bde38fa3000ac612da7ea242205f688cd22b59
Hao Zhang <hzhangxyz@outlook.com>
2021-12-21 13:08:30 +0800

    [WIP] add mpi4py

--- example/sample-mps-heisenberg.py
+++ example/sample-mps-heisenberg.py
@@ -1,4 +1,5 @@
 import pickle
+from mpi4py import MPI
 import TAT
 from han.systems.abstract_system import AbstractSamplingSystem
 from han.systems.mps_eom_with_x4_post import MPS_EOM_with_x4_post
@@ -20,22 +21,42 @@ def create(file_name, depth, length, D, Dc, seed):
         pickle.dump(lattice, file)
 
 
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+size = comm.Get_size()
+
+
+def _sum_dict(d1, d2):
+    return {k: d1[k] + d2[k] for k in d1}
+
+
+def _mpi_mean(gp):
+    result = comm.allreduce(gp, _sum_dict)
+    return {k: v / size for k, v in result.items()}
+
+
 def update(file_name, count, step, sampling, seed):
     with open(file_name, "rb") as file:
         lattice = pickle.load(file)
-    TAT.random.seed(seed)
+    TAT.random.seed(seed + rank)
     gen01 = TAT.random.uniform_real(0, 1)
     for t in range(count):
         ss = lattice.get_configurations(gen01, sampling)
         e, branchs = lattice.energy(ss)
-        print(t, e / lattice.L2)
-        with open(file_name.replace(".dat", "") + ".log", "a") as file:
-            print(t, e / lattice.L2, file=file)
         gp = lattice.grad_of_param(ss, e, branchs)
+        gp["e"] = e
+
+        gp = _mpi_mean(gp)
+
         for k in gp:
-            lattice.parameter[k] -= float(step) * gp[k]
-        with open(file_name, "wb") as file:
-            pickle.dump(lattice, file)
+            if k != "e":
+                lattice.parameter[k] -= float(step) * gp[k]
+        if rank == 0:
+            print(t, gp["e"] / lattice.L2)
+            with open(file_name.replace(".dat", "") + ".log", "a") as file:
+                print(t, gp["e"] / lattice.L2, file=file)
+            with open(file_name, "wb") as file:
+                pickle.dump(lattice, file)
 
 
 if __name__ == "__main__":
