14d7a0c630b04d82e8bc0abdfea0580fabe97cb3
Hao Zhang <hzhangxyz@outlook.com>
2023-03-06 19:12:07 +0800

    add test for cz

---
+++ example/mps-ising-nopost.py
@@ -0,0 +1,50 @@
+import pickle
+import TAT
+from han.systems.abstract_system import AbstractHoleSystem, r_bound
+from han.systems.mps_eom_without_post import MPS_EOM_without_post
+from han.systems.ising import Ising
+
+
+class MPS_Ising(Ising, MPS_EOM_without_post, AbstractHoleSystem):
+    pass
+
+
+def create(file_name, depth, length, D, Dc, seed):
+    lattice = MPS_Ising(depth=depth,
+                             length=length,
+                             D=D,
+                             Dc=Dc,
+                             Tensor=TAT.No.D.Tensor)
+
+    lattice.generate_initial_state(seed)
+    with open(file_name, "wb") as file:
+        pickle.dump(lattice, file)
+
+
+def show(file_name):
+    with open(file_name, "rb") as file:
+        lattice = pickle.load(file)
+    for k, v in lattice.parameter.param.items():
+        print(k, v())
+
+
+def update(file_name, count, step):
+    with open(file_name, "rb") as file:
+        lattice = pickle.load(file)
+    for t in range(count):
+        a, b = lattice._energies()
+        print(t, a / b / lattice.L2)
+        with open(file_name.replace(".dat", "") + ".log", "a") as file:
+            print(t, a / b / lattice.L2, file=file)
+        gp = lattice.grad_of_param()
+        for k in gp:
+            lattice.parameter[k] -= float(step) * gp[k]
+        lattice.refine_parameters()
+        with open(file_name, "wb") as file:
+            pickle.dump(lattice, file)
+
+
+if __name__ == "__main__":
+    import fire
+    fire.core.Display = lambda lines, out: out.write("\n".join(lines) + "\n")
+    fire.Fire()
---
+++ han/systems/mps_eom_without_post.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright (C) 2020-2021 Hao Zhang<zh970205@mail.ustc.edu.cn>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+import TAT
+import lazy
+from ..utility.storage_function import StorageFunction
+from .abstract_system import AbstractSystem, r_bound
+from ..utility.tensor_U import tensor_U
+
+
+@StorageFunction
+def get_U(Tensor, n, r, omega, shrink=tuple()):
+    result = tensor_U(n, n, r, omega, 0, 0).edge_rename({
+        "I1": "L",
+        "O1": "R",
+        "O2": "D",
+        "I2": "U"
+    }).shrink({i: 0 for i in shrink})
+    if Tensor == TAT.No.D.Tensor:
+        return result.to(float)
+    elif Tensor == TAT.No.S.Tensor:
+        return result.to("float32")
+    elif Tensor == TAT.No.C.Tensor:
+        return result.to("complex64")
+    elif Tensor == TAT.No.Z.Tensor:
+        return result
+    else:
+        raise ValueError("invalid tensor type")
+
+
+class MPS_EOM_without_post(AbstractSystem):
+
+    def __init__(self, depth, length, D, Dc, Tensor):
+        super(MPS_EOM_with_x4_post, self).__init__(depth + 1, length, Dc,
+                                                   Tensor)
+        self.D = D
+
+        self._construct_params()
+        self._construct_tensors()
+
+    def _construct_params(self):
+        for l1 in range(self.L1 - 1):
+            for l2 in range(self.L2):
+                self.parameter.add(("r", l1, l2))
+                self.parameter.add(("omega", l1, l2))
+
+    def _construct_tensors(self):
+        for l1 in range(self.L1):
+            for l2 in range(self.L2):
+                self._construct_tensor(l1, l2)
+
+    def _construct_projector_tensor(self):
+        projector = self.Tensor(["D", "U"], [self.d, self.D]).zero()
+        for e in range(2):
+            projector[{"D": e, "U": e}] = 1
+        return projector
+
+    def _construct_normal_tensor(self, l1, l2, r, omega):
+        shrink = set()
+        if l1 == 0:
+            shrink.add("U")
+        if l2 == 0:
+            shrink.add("L")
+        if l2 == self.L2 - 1:
+            shrink.add("R")
+        result = get_U(self.Tensor, self.D, r, omega, tuple(shrink))
+        return result
+
+    def _construct_tensor(self, l1, l2):
+        if l1 == self.L1 - 1:
+            self.tensor[l1][l2].replace(
+                lazy.Node(self._construct_projector_tensor))
+        else:
+            self.tensor[l1][l2].replace(
+                lazy.Node(self._construct_normal_tensor, l1, l2,
+                          self.parameter.param["r", l1, l2],
+                          self.parameter.param["omega", l1, l2]))
+
+    def _modified_tensor(self, key):
+        if len(key) == 3:
+            romega, l1, l2 = key
+            return [(l1, l2)]
+        if len(key) == 4:
+            _, l2, ed, e4 = key
+            return [(self.L1 - 1, l2)]
+
+    def generate_initial_state(self, seed):
+        TAT.random.seed(seed)
+        uni1 = TAT.random.uniform_real(-1, +1)
+        unir = TAT.random.uniform_real(-r_bound, +r_bound)
+        unipi = TAT.random.uniform_real(-3.14, +3.14)
+
+        for l1 in range(self.L1 - 1):
+            for l2 in range(self.L2):
+                self.parameter["r", l1, l2] = unir()
+                self.parameter["omega", l1, l2] = unipi()
+
+    def refine_parameters(self):
+        for l1 in range(self.L1 - 1):
+            for l2 in range(self.L2):
+                if self.parameter["r", l1, l2] > +r_bound:
+                    self.parameter["r", l1, l2] = +r_bound
+                if self.parameter["r", l1, l2] < -r_bound:
+                    self.parameter["r", l1, l2] = -r_bound
