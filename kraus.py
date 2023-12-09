6c0d8d157e2a25fc6ceb98aef0b2fa2b85deaf18
Hao Zhang <hzhangxyz@outlook.com>
2023-12-09 12:18:59 +0800

    Fix kraus

--- kraus.py
+++ kraus.py
@@ -13,11 +13,11 @@ def factor(k):
 def kraus(D, k, eta):
     result = Tensor(["k", "O", "I"], [k, D, D]).zero()
     for i in range(k):
-        param = ((1 - eta)**i / factor(i))**(1 / 2)
         for a in range(D):
             b = a - i
             if b >= 0:
-                result[{"k": i, "I": a, "O": b}] = param * eta**(b / 2)
+                param = ((1 - eta)**(i / 2)) * (factor(a) / factor(i) / factor(a - i))**(1 / 2)
+                result[{"k": i, "I": a, "O": b}] = eta**(b / 2) * param
     return result
 
 
